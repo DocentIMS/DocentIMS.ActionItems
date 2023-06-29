b61680555794f01bb19912cc3800a36f1d346154
Espen Moe-Nilssen <espen@medialog.no>
2023-06-29 16:48:14 +0200

    Add historyview and 'moments date' in view template

--- src/DocentIMS/ActionItems/views/action_items_view.py
+++ src/DocentIMS/ActionItems/views/action_items_view.py
@@ -4,6 +4,8 @@
 from Products.Five.browser import BrowserView
 from zope.interface import Interface
 from plone import api
+import DateTime
+import datetime
 
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -23,3 +25,8 @@ class ActionItemsView(BrowserView):
 
     def portal_url(self):
         return api.portal.get().absolute_url()
+
+    def due_date(self):
+        return self.context.created().ISO()
+        #return datetime.datetime.from_date(initial_due_date).ISO()
+        #return self.context.initial_due_date().ISO()
