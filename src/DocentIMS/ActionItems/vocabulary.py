317ee4f8af9f776b716863363b7f3cae6792a52e
Espen Moe-Nilssen <espen@medialog.no>
2023-07-05 20:30:53 +0200

    Added Vocabulary

---
+++ src/DocentIMS/ActionItems/vocabulary.py
@@ -0,0 +1,19 @@
+from zope.interface import directlyProvides
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+from zope.schema.interfaces import IVocabularyFactory
+from plone import api
+
+#from zope.i18nmessageid import MessageFactory
+#_ = MessageFactory('DocentIMS.ActionItems')
+
+def format_title(folder):
+    return "{}  ...   [ {} ]".format( folder.Title, folder.getURL())
+
+
+def ShowActionItemsVocabulary(context):
+    items = api.content.find(portal_type=['sow_analysis', 'SOW Analysis'], sort_on='sortable_title')
+    if items:
+        terms = [ SimpleTerm(value=item.UID, token=item.UID, title=item.Title) for item in items ]
+    return SimpleVocabulary(terms)
+
+directlyProvides(ShowActionItemsVocabulary, IVocabularyFactory)
