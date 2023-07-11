18467627a1b25a13ed381bdaaf3265d83ee43a79
Espen Moe-Nilssen <espen@medialog.no>
2023-07-11 17:17:53 +0200

    Update action_items.js

--- src/DocentIMS/ActionItems/browser/static/js/action_items.js
+++ src/DocentIMS/ActionItems/browser/static/js/action_items.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
   if( $('#form-widgets-is_this_action_out_of_the_scope_of_work_-0').not(':checked') ) {
-      $('#formfield-form-widgets-explanation_for_out_of_scope_ai, #formfield-form-widgets-related_sow_section, #formfield-form-widgets-sow_section_text').hide();
+      $('#formfield-form-widgets-explanation_for_out_of_scope_ai').hide();
+      $('#formfield-form-widgets-related_sow_section, #formfield-form-widgets-sow_section_text').show();
   }
   $('#formfield-form-widgets-is_this_action_out_of_the_scope_of_work_ input').change(function () {
       if( $('#form-widgets-is_this_action_out_of_the_scope_of_work_-0').is(':checked') ) {
-          $('#formfield-form-widgets-explanation_for_out_of_scope_ai, #formfield-form-widgets-related_sow_section, #formfield-form-widgets-sow_section_text').show();
+          $('#formfield-form-widgets-explanation_for_out_of_scope_ai').show();
+          $('#formfield-form-widgets-related_sow_section, #formfield-form-widgets-sow_section_text').hide();
       }
       else {
-        $('#formfield-form-widgets-explanation_for_out_of_scope_ai, #formfield-form-widgets-related_sow_section, #formfield-form-widgets-sow_section_text').hide();
+        $('#formfield-form-widgets-explanation_for_out_of_scope_ai').hide();
+        $('#formfield-form-widgets-related_sow_section, #formfield-form-widgets-sow_section_text').show();
       }
   });
   if( $('#form-widgets-is_this_item_closed-0 input').not(':checked') ) {
