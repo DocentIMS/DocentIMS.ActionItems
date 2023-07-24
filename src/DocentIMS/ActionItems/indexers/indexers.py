1cdff56ff633f3b6a43880f5e2627a98ce77f4ec
Espen Moe-Nilssen <espen@medialog.no>
2023-07-24 15:45:54 +0200

    Update indexers.py

--- src/DocentIMS/ActionItems/indexers/indexers.py
+++ src/DocentIMS/ActionItems/indexers/indexers.py
@@ -6,7 +6,7 @@ from plone.dexterity.interfaces import IDexterityContainer
 from plone.indexer import indexer
 
 
-@indexer(IDexterityContainer)
+@indexer(IDexterityContent)
 def dummy(obj):
     """ Dummy to prevent indexing other objects thru acquisition """
     raise AttributeError('This field should not indexed here!')
@@ -16,15 +16,17 @@ def dummy(obj):
 def actionIndexer(obj):
     """Calculate and return the value for the indexer"""
     #import pdb; pdb.set_trace()
-    if "action_items-" in obj.id:
-        action_item =  obj.id.replace('action_items', '').replace("-", '')
-        action_item_no = int(float(action_item))
-        return action_item_no
+    if obj.portal_type== 'action_items':
+        if "action_items-" in obj.id:
+            action_item =  obj.id.replace('action_items', '').replace("-", '')
+            action_item_no = int(float(action_item))
+            return action_item_no
     return None
 
 
 @indexer(IDexterityContainer)  # ADJUST THIS!
 def priorityIndexer(obj):
     """Calculate and return the value for the indexer"""
-    if obj.priority:
-        return int(float(obj.priority))
+    if obj.portal_type== 'action_items':
+        if obj.priority:
+            return int(float(obj.priority))
