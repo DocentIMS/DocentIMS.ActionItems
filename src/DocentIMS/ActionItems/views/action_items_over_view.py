fdfa67020f31d5ba9de75d636738f8cbcef12114
Espen Moe-Nilssen <espen@medialog.no>
2023-08-01 14:53:20 +0200

    Added plotly graphs for importantce and days left.

--- src/DocentIMS/ActionItems/views/action_items_over_view.py
+++ src/DocentIMS/ActionItems/views/action_items_over_view.py
@@ -6,6 +6,7 @@ from plone import api
 from ..interfaces import IDocentimsSettings
 import DateTime
 import datetime
+from datetime import timezone
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
@@ -21,3 +22,35 @@ class ActionItemsOverView(BrowserView):
     def today(self):
         #import pdb; pdb.set_trace()
         return datetime.date.today()
+
+    def get_graphdata(self):
+        colors = ['#CCCCCC',  '#FF0000',  'orange', '#123456']
+        items = self.context.items()
+        datanames = [item[1].Title() for item in items]
+        datavalues = [item[1].priority or 0 for item in items]
+        datacolors = [ colors[datavalue] for datavalue in datavalues]
+        return datanames, datavalues, datacolors
+
+
+    def get_piedata(self):
+        colors = ['#CCCCCC',  '#FF0000',  'orange', '#123456']
+        today = datetime.date.today()
+        now = datetime.datetime.now(timezone.utc)
+        items = self.context.items()
+
+        itemlist = []
+        index = 0
+        contentitems = [item[1] for item in items]
+
+        for item in contentitems:
+            title = item.Title()
+            duedate = item.duedate or today
+            created = item.created()
+            delta = duedate - today
+            delta2 =  now -  created.asdatetime()
+            #delta3 =  duedate -  created.asdatetime()
+            itemlist.append({'title': title, 'data': [delta.days, delta2.days], 'index': index})
+            index += 1
+
+        #import pdb; pdb.set_trace()
+        return itemlist
