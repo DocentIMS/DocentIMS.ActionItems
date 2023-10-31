1397143bbf056443ca130e7237cd7a8ad7061332
Espen Moe-Nilssen <espen@medialog.no>
2023-10-31 18:37:31 +0100

    TinyMCE link button code

--- src/DocentIMS/ActionItems/browser/static/tiny_mce/plugins/index.js
+++ src/DocentIMS/ActionItems/browser/static/tiny_mce/plugins/index.js
@@ -3,7 +3,7 @@ tinymce.PluginManager.add("link_plugin", function (editor) {
     icon: "duplicate",
     onAction: () => {
       var content = editor.selection.getContent({ format: "text" });
-      let tempValue = `<span style="background: yellow">${content}</span>`;
+      let tempValue = `<span style="background: orange">${content}</span>`;
       if (content?.length) editor.selection.setContent(tempValue);
     },
   });
