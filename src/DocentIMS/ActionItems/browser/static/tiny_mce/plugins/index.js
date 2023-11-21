49864d7e27b1453b3927e3cf5b672f637da14469
Espen Moe-Nilssen <espen@medialog.no>
2023-11-21 13:31:59 +0100

    Installed plugin, but gives error nabvigator.clipboard.writeText

--- src/DocentIMS/ActionItems/browser/static/tiny_mce/plugins/index.js
+++ src/DocentIMS/ActionItems/browser/static/tiny_mce/plugins/index.js
@@ -1,10 +1,19 @@
-tinymce.PluginManager.add("link_plugin", function (editor) {
-  editor.ui.registry.addButton("link_btn", {
-    icon: "duplicate",
+tinymce.PluginManager.add("DocentIMS_CreateAI", function (editor) {
+  editor.ui.registry.addButton("DocentIMS_CreateAI_btn", {
+    icon: "comment-add",
     onAction: () => {
       var content = editor.selection.getContent({ format: "text" });
-      let tempValue = `<span style="background: orange">${content}</span>`;
-      if (content?.length) editor.selection.setContent(tempValue);
+      let tempValue = `<span style="background: yellow">${content}</span>`;
+      navigator.clipboard.writeText(content).then(() => {
+        if (content?.length) {
+          editor.selection.setContent(tempValue);
+          setTimeout(() => {
+            const linkToOpen =
+              "http://team.reverebeachproject.com/action-items/++add++action_items";
+            window.open(linkToOpen, "_blank");
+          }, 250);
+        }
+      });
     },
   });
 });
