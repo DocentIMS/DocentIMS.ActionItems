926f6a6180637110114b95af26e437ded0be0554
Espen Moe-Nilssen <espen@medialog.no>
2023-12-06 18:16:22 +0100

    Update index.js

--- src/DocentIMS/ActionItems/browser/static/tiny_mce/plugins/index.js
+++ src/DocentIMS/ActionItems/browser/static/tiny_mce/plugins/index.js
@@ -7,7 +7,7 @@ tinymce.PluginManager.add("DocentIMS_CreateAI", function (editor) {
       var baseUrl = body.getAttribute('data-base-url');
       var portalUrl = body.getAttribute('data-portal-url');
       var to_uuid = "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
-      let tempValue = `<a href="a href="../resolveuid/${to_uuid}"><span style="background: yellow">${content}</span></a>`;
+      let tempValue = `<a href="../resolveuid/${to_uuid}"><span style="background: yellow">${content}</span></a>`;
       // console.log(tempValue);
       navigator.clipboard.writeText(content).then(() => {
         if (content?.length) {
