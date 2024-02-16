12a9b73b34dc0e04282057ab8a213e86b29d3a55
Espen Moe-Nilssen <espen@medialog.no>
2024-02-16 13:40:40 +0100

    Viewlet for color

--- src/DocentIMS/ActionItems/viewlets/css_viewlet.py
+++ src/DocentIMS/ActionItems/viewlets/css_viewlet.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from plone.app.layout.viewlets import ViewletBase
+from plone import api
 
 
 class CSSViewlet(ViewletBase):
 
-    def update(self):
-        self.message = self.get_message()
-
-    def get_message(self):
-        return u'My message'
-
+    def color(self):
+    	return api.portal.get_registry_record('DocentIMS.ActionItems.interfaces.IDocentimsSettings.color1')
+        
     def index(self):
         return super(CSSViewlet, self).render()
+
+ 
+    
