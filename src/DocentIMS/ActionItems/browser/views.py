6703f443a223ece72dae20f9e1963dcc66a0b5c8
Espen Moe-Nilssen <espen@medialog.no>
2024-03-18 14:23:23 +0100

    Update views.py

--- src/DocentIMS/ActionItems/browser/views.py
+++ src/DocentIMS/ActionItems/browser/views.py
@@ -2,8 +2,8 @@
 
 
 #from zope.publisher.browser import BrowserView
-        from plone.dexterity.browser.add import DefaultAddView
-        from plone.dexterity.browser.add import DefaultAddForm
+from plone.dexterity.browser.add import DefaultAddView
+from plone.dexterity.browser.add import DefaultAddForm
 from plone.dexterity.browser.edit import DefaultEditView
 from plone.dexterity.browser.edit import DefaultEditForm
 
