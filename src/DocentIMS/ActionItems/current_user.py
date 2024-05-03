b7c071cd50cf3cd18bc6f3f12b50b9af02b566b0
Espen Moe-Nilssen <espen@medialog.no>
2024-05-03 11:45:49 +0200

    Create current_user.py

---
+++ src/DocentIMS/ActionItems/current_user.py
@@ -0,0 +1,16 @@
+from zope.interface import provider
+from zope.schema.interfaces import IContextAwareDefaultFactory
+from plone import api
+
+
+@provider(IContextAwareDefaultFactory)
+def getUserId(context):
+    current_user = api.user.get_current()
+    user_groups =  current_user.getGroups()
+    if 'AuthenticatedUsers' in user_groups:
+        return current_user.id
+    return None
+
+
+
+ 
