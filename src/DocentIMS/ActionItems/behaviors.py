5adaa23f767adcd12ef0854fae2787c59438affc
Espen Moe-Nilssen <espen@medialog.no>
2024-05-08 18:01:24 +0200

    event Meeting overrides of behaviors

--- src/DocentIMS/ActionItems/behaviors.py
+++ src/DocentIMS/ActionItems/behaviors.py
@@ -1,7 +1,7 @@
 from plone.app.event.dx.behaviors import IEventLocation
 from plone.autoform import directives
 
-from plone.app.event.dx.behaviors import IEventLocation
+from plone.app.event.dx.behaviors import IEventLocation, IEventAttendees, IEventContact
 from plone.app.z3cform.widget import AjaxSelectFieldWidget
 from zope import schema
 from zope.interface import provider
@@ -20,3 +20,30 @@ class IMeetingLocation(IEventLocation):
     directives.widget("location", AjaxSelectFieldWidget, klass="event_location")
 
  
+@provider(IFormFieldProvider)
+class IMeetingContact( IEventContact):
+    """Event Contact Schema."""
+
+    contact_name = schema.Choice(
+        title="Contact Person", 
+        required=False,
+        vocabulary= 'DocentIMS.ActionItems.FullnamesVocabulary'
+    )
+    directives.widget("contact_name", AjaxSelectFieldWidget, klass="event_contact_name")
+
+
+@provider(IFormFieldProvider)
+class IMeetingAttendees(IEventAttendees):
+    """MEETING Attendees Schema."""
+
+    attendees = schema.List(
+        title= "Attendees",
+        description= "List of attendees.",
+        value_type=schema.Choice(
+            title="Attendee",
+            required=False,
+            vocabulary=  'DocentIMS.ActionItems.FullnamesVocabulary'
+            
+        )
+    )
+    directives.widget("attendees",  AjaxSelectFieldWidget, vocabulary= 'DocentIMS.ActionItems.FullnamesVocabulary',  klass="event_attendees")
