665114d8c6328594b654911435b2621001615f62
Espen Moe-Nilssen <espen@medialog.no>
2024-07-12 12:49:38 +0200

    Added behaviors for openoffice and fixed actions etc. Needs 1 fix in openoffice to work

--- src/DocentIMS/ActionItems/behaviors/only_office_file.py
+++ src/DocentIMS/ActionItems/behaviors/only_office_file.py
@@ -1,44 +1,49 @@
 # -*- coding: utf-8 -*-
 
 from DocentIMS.ActionItems import _
-from plone import schema
-from plone.autoform.interfaces import IFormFieldProvider
-from plone.supermodel import model
-from Products.CMFPlone.utils import safe_hasattr
 from zope.component import adapter
 from zope.interface import Interface
-from zope.interface import implementer
-from zope.interface import provider
+#from plone import schema
+#from plone.autoform.interfaces import IFormFieldProvider
+#from Products.CMFPlone.utils import safe_hasattr
+#from zope.interface import implementer
+#from zope.interface import provider
 
+#from plone.autoform import directives
+
+#>>> from zope.interface import Interface
+#>>> from plone.namedfile import field
+#>>> from plone.namedfile.interfaces import HAVE_BLOBS
+#from plone.namedfile.field import NamedBlobFile
 
 class IOnlyOfficeFileMarker(Interface):
     pass
 
 
-@provider(IFormFieldProvider)
-class IOnlyOfficeFile(model.Schema):
-    """
-    """
+# @provider(IFormFieldProvider)
+#class IOnlyOfficeFile(model.Schema):
+#     """
+#     """
 
-    project = schema.TextLine(
-        title=_(u'Project'),
-        description=_(u'Give in a project name'),
-        required=False,
-    )
+#     # file = NamedBlobFile(
+#     #     title=_(u'File'),
+#     #     description=_(u'File can be used with onlyoffice'),
+#     #     required=False,
+#     # )
 
 
-@implementer(IOnlyOfficeFile)
+#@implementer(IOnlyOfficeFile)
 @adapter(IOnlyOfficeFileMarker)
 class OnlyOfficeFile(object):
     def __init__(self, context):
         self.context = context
 
-    @property
-    def project(self):
-        if safe_hasattr(self.context, 'project'):
-            return self.context.project
-        return None
+    # @property
+    # def project(self):
+    #     if safe_hasattr(self.context, 'file'):
+    #         return self.context.file
+    #     return None
 
-    @project.setter
-    def project(self, value):
-        self.context.project = value
+    # @project.setter
+    # def project(self, value):
+    #     self.context.file = value
