f808a4fbb8ef50ddcd7c49038f8c945c01e04d76
Espen Moe-Nilssen <espen@medialog.no>
2024-08-27 17:33:26 +0200

    folder tabbed view

--- src/DocentIMS/ActionItems/views/documents_folder_view.py
+++ src/DocentIMS/ActionItems/views/documents_folder_view.py
@@ -3,6 +3,8 @@
 # from DocentIMS.ActionItems import _
 from Products.Five.browser import BrowserView
 from zope.interface import Interface
+from Products.CMFCore.utils import getToolByName
+from plone import api
 
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -25,30 +27,14 @@ class DocumentsFolderView(BrowserView):
     #     batch = self.context.restrictedTraverse('@@contentlisting')(sort_on='sortable_title', batch=True, b_size=400);
     #     return batch
     
-    def batch(self):
-        # Retrieve the content listing with batching
-        # batch = self.context.restrictedTraverse('@@contentlisting')(sort_on='sortable_title', batch=True, b_size=400)
+    def get_types(self):
+        folder = self.context  # Assuming the script is created inside the folder
+        portal_types = [item.portal_type for item in folder.objectValues()]
+
+        return set(portal_types)
         
-        items = self.context.getFolderContents()
-        
-        return items
-
-        # Initialize a dictionary to store items grouped by portal type
-        grouped_items = {}
-
-        # Loop through each item in the batch
-        for item in items:
-            # Get the portal type of the current item
-            portal_t = item.portal_type
-
-            # If this portal type isn't in the dictionary yet, add it with an empty list
-            if portal_t not in grouped_items:
-                grouped_items[portal_t] = []
-
-            # Append the current item to the list for this portal type
-            grouped_items[portal_t].append({title: item.Title()})
-
-        # Return the grouped items dictionary
-        return grouped_items
+    def batch(self, contentype):
+        batch = self.context.restrictedTraverse('@@contentlisting')(sort_on='sortable_title',  portal_type=contentype, batch=True, b_size=400)
+        return batch
  
  
