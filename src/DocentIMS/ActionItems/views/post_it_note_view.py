a17cb5e2592ffb05a23b8b55f8908fdec3ea9064
Espen <espen@medialog.no>
2024-09-18 10:56:15 +0000

    "Add view: post-it-note-view"

---
+++ src/DocentIMS/ActionItems/views/post_it_note_view.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# from DocentIMS.ActionItems import _
+from Products.Five.browser import BrowserView
+from zope.interface import Interface
+
+# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class IPostItNoteView(Interface):
+    """ Marker Interface for IPostItNoteView"""
+
+
+class PostItNoteView(BrowserView):
+    # If you want to define a template here, please remove the template from
+    # the configure.zcml registration of this view.
+    # template = ViewPageTemplateFile('post_it_note_view.pt')
+
+    def __call__(self):
+        # Implement your own actions:
+        return self.index()
