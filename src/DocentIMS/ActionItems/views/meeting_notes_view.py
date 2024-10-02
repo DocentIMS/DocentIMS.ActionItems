791349ca08d669c6c7338fac6828b5edc76f5b69
Espen Moe-Nilssen <espen@medialog.no>
2024-10-02 12:15:22 +0200

    Board Meeting Notes

--- src/DocentIMS/ActionItems/views/meeting_notes_view.py
+++ src/DocentIMS/ActionItems/views/meeting_notes_view.py
@@ -3,6 +3,7 @@
 # from DocentIMS.ActionItems import _
 from Products.Five.browser import BrowserView
 from zope.interface import Interface
+from plone import api
 
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -19,3 +20,14 @@ class MeetingNotesView(BrowserView):
     def __call__(self):
         # Implement your own actions:
         return self.index()
+    
+    def get_attended(self):
+        namelist = []
+        userlist = self.context.which_board_members_attended
+        if userlist:
+            for userid in userlist:
+                user = api.user.get(userid=userid,)
+                namelist.append(user.getProperty('fullname'))
+            
+            return namelist
+        return None
