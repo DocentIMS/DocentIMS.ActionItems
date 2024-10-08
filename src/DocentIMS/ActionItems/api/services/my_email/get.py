f50e28b775e9392a8d96ba753f58ad1dbb3bef1a
Espen Moe-Nilssen <espen@medialog.no>
2024-10-08 19:54:53 +0200

    Update get.py

--- src/DocentIMS/ActionItems/api/services/my_email/get.py
+++ src/DocentIMS/ActionItems/api/services/my_email/get.py
@@ -40,7 +40,7 @@ class MyEmail(object):
               
         if user is not None: 
             my_groups = user.getGroups() or None
-            members = []
+            members = None
             if my_groups:   
                
                 for mygroup in my_groups:
