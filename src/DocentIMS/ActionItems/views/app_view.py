a80ec93743f2b09aa0bd714e6633bc3e3c8724c3
Espen Moe-Nilssen <espen@medialog.no>
2024-10-23 12:06:13 +0200

    meeting stuff

--- src/DocentIMS/ActionItems/views/app_view.py
+++ src/DocentIMS/ActionItems/views/app_view.py
@@ -3,6 +3,7 @@
 # from DocentIMS.ActionItems import _
 from Products.Five.browser import BrowserView
 from zope.interface import Interface
+# from Products.CMFCore.utils import getToolByName
 
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -19,3 +20,38 @@ class AppView(BrowserView):
     def __call__(self):
         # Implement your own actions:
         return self.index()
+    
+    def len_ais(self):
+        my_brains = self.context.portal_catalog(portal_type=['action_items'])
+        
+        if my_brains:        
+            return len(my_brains)
+    
+    def count_ais(self):
+        urgencies = self.context.portal_catalog.uniqueValuesFor("urgency")
+        if urgencies:
+            urgency_list = []
+            for urgency in urgencies:
+                my_brains = self.context.portal_catalog(portal_type=['action_items'], urgency=urgency)
+                urgency_list.append({'name': urgency, 'count': len(my_brains)})
+            return urgency_list
+            
+        return None
+
+
+    def len_meetings(self):
+        # urgencies = self.context.portal_catalog.uniqueValuesFor("meeting_types")
+        my_brains = self.context.portal_catalog(portal_type=['meeting', 'Meeting'] )
+        return len(my_brains)
+        # return None
+        
+    def count_meetings(self):
+        meeting_types = self.context.portal_catalog.uniqueValuesFor("meeting_type")
+        if meeting_types:
+            meeting_list = []
+            for meeting_type in meeting_types:
+                my_brains = self.context.portal_catalog(portal_type=['meeting', 'Meeting'], meeting_type=meeting_type)
+                meeting_list.append({'name': meeting_type, 'count': len(my_brains)})
+            return meeting_list
+            
+        return None
