fa902d5ecaefcc6af53e95baed1eb8d885d6e7cc
Espen Moe-Nilssen <espen@medialog.no>
2024-11-01 16:56:00 +0100

    Update get.py

--- src/DocentIMS/ActionItems/api/services/docs_info/get.py
+++ src/DocentIMS/ActionItems/api/services/docs_info/get.py
@@ -102,7 +102,7 @@ class DocsInfo(object):
             result = {
                 'docs_info': {
                     'meeting_locations': api.portal.get_registry_record('DocentIMS.ActionItems.interfaces.IDocentimsSettings.location_names'),
-                    'meeting_types':  json.dumps(api.portal.get_registry_record('DocentIMS.ActionItems.interfaces.IDocentimsSettings.meeting_types')), 
+                    # 'meeting_types':  json.dumps(api.portal.get_registry_record('DocentIMS.ActionItems.interfaces.IDocentimsSettings.meeting_types')), 
                     'planning_project': api.portal.get_registry_record('DocentIMS.ActionItems.interfaces.IDocentimsSettings.planning_project'),
                     'project_color': api.portal.get_registry_record('DocentIMS.ActionItems.interfaces.IDocentimsSettings.color1'),
                     'marking_color': api.portal.get_registry_record('DocentIMS.ActionItems.interfaces.IDocentimsSettings.color2'),
