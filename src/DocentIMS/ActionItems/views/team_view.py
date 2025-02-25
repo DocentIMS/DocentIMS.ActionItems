ec0f2d2c39c3a0dbde71ed1043c2a2b067ce6781
Espen Moe-Nilssen <espen@medialog.no>
2025-02-25 19:20:37 +0100

    adding table header

--- src/DocentIMS/ActionItems/views/team_view.py
+++ src/DocentIMS/ActionItems/views/team_view.py
@@ -4,6 +4,8 @@
 from Products.Five.browser import BrowserView
 from zope.interface import Interface
 
+from plone import api
+
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
@@ -19,3 +21,13 @@ class TeamView(BrowserView):
     def __call__(self):
         # Implement your own actions:
         return self.index()
+    
+    def get_group_members(self):
+        group_id = "PrjTeam"
+        users = api.group.get(group_id=group_id).getMembers()
+        # userlist = []
+        
+        return [{'name': user.getProperty('fullname') or user.getId(), 'role': user.getProperty('your_team_role'), 'company': user.getProperty('company'), } for user in users]
+
+
+
