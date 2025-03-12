231dc2b6fbc2ce7cb915175e1774ed2156b1117a
Espen Moe-Nilssen <espen@medialog.no>
2025-03-12 12:24:26 +0100

    Noconflict JS and hide member Tag at bottom of page

--- src/DocentIMS/ActionItems/browser/static/js/action_items.js
+++ src/DocentIMS/ActionItems/browser/static/js/action_items.js
@@ -1,77 +1,75 @@
+var $jq = jQuery.noConflict();
+$jq(document).ready(function () {
+
+  // Scope analysis, hide show fields. 
+  // Shows and hides fields depending on checkbox
+
+  if ($jq('#form-widgets-internal_qc_required_-0').is(':checked')) {
+    // $('#formfield-form-widgets-estimated_qc_time, #formfield-form-widgets-notes_to_qc').show();
+  } else {
+    $jq('#formfield-form-widgets-estimated_qc_time, #formfield-form-widgets-notes_to_qc, #formfield-form-widgets-finishing_date_for_qc, #formfield-form-widgets-date_wc_finished, #formfield-form-widgets-date_item_finished').hide();
+  }
+
+  $jq('#form-widgets-internal_qc_required_-0').change(function () {
+    $jq('#formfield-form-widgets-estimated_qc_time, #formfield-form-widgets-notes_to_qc, #formfield-form-widgets-finishing_date_for_qc, #formfield-form-widgets-date_wc_finished, #formfield-form-widgets-date_item_finished').toggle();
+  });
 
 
-$(document).ready(function() {
-
-
-// Scope analysis, hide show fields. 
-// Shows and hides fields depending on checkbox
-
-if( $('#form-widgets-internal_qc_required_-0').is(':checked') ) {
-  // $('#formfield-form-widgets-estimated_qc_time, #formfield-form-widgets-notes_to_qc').show();
-} else {
-  $('#formfield-form-widgets-estimated_qc_time, #formfield-form-widgets-notes_to_qc, #formfield-form-widgets-finishing_date_for_qc, #formfield-form-widgets-date_wc_finished, #formfield-form-widgets-date_item_finished').hide();
-}
-
-$('#form-widgets-internal_qc_required_-0').change(function () {
-  $('#formfield-form-widgets-estimated_qc_time, #formfield-form-widgets-notes_to_qc, #formfield-form-widgets-finishing_date_for_qc, #formfield-form-widgets-date_wc_finished, #formfield-form-widgets-date_item_finished').toggle();
-});
-
-
-if( $('#form-widgets-is_the_analyis_complete-0').is(':checked') ) {
+  if ($jq('#form-widgets-is_the_analyis_complete-0').is(':checked')) {
     //$('#formfield-form-widgets-analysis_notes').show();
-} else {
-  $('#formfield-form-widgets-analysis_notes').hide();
-}
+  } else {
+    $jq('#formfield-form-widgets-analysis_notes').hide();
+  }
 
-$('#form-widgets-is_the_analyis_complete-0').change(function () {
-  $('#formfield-form-widgets-analysis_notes').toggle();
-});
+  $jq('#form-widgets-is_the_analyis_complete-0').change(function () {
+    $jq('#formfield-form-widgets-analysis_notes').toggle();
+  });
 
-// end
+  // end
 
-  if( $('#form-widgets-is_this_action_out_of_the_scope_of_work_-0').not(':checked') ) {
-      $('#formfield-form-widgets-explanation_for_out_of_scope_ai').hide();
+  if ($jq('#form-widgets-is_this_action_out_of_the_scope_of_work_-0').not(':checked')) {
+    $jq('#formfield-form-widgets-explanation_for_out_of_scope_ai').hide();
 
   } else {
-    $('#formfield-form-widgets-related_sow_section, #sow_section_text').hide();
+    $jq('#formfield-form-widgets-related_sow_section, #sow_section_text').hide();
   }
-  $('#formfield-form-widgets-is_this_action_out_of_the_scope_of_work_ input').change(function () {
-      if( $('#form-widgets-is_this_action_out_of_the_scope_of_work_-0').is(':checked') ) {
-          $('#formfield-form-widgets-explanation_for_out_of_scope_ai').show();
-          $('#formfield-form-widgets-related_sow_section, #section_text').hide();
-      }
-      else {
-        $('#formfield-form-widgets-explanation_for_out_of_scope_ai').hide();
-        $('#formfield-form-widgets-related_sow_section, #sow_section_text').show();
-      }
+  $jq('#formfield-form-widgets-is_this_action_out_of_the_scope_of_work_ input').change(function () {
+    if ($jq('#form-widgets-is_this_action_out_of_the_scope_of_work_-0').is(':checked')) {
+      $jq('#formfield-form-widgets-explanation_for_out_of_scope_ai').show();
+      $jq('#formfield-form-widgets-related_sow_section, #section_text').hide();
+    }
+    else {
+      $jq('#formfield-form-widgets-explanation_for_out_of_scope_ai').hide();
+      $jq('#formfield-form-widgets-related_sow_section, #sow_section_text').show();
+    }
   });
-  if( $('#form-widgets-is_this_item_closed-0 input').not(':checked') ) {
-      $('#formfield-form-widgets-actions_to_close_out').hide();
+  if ($jq('#form-widgets-is_this_item_closed-0 input').not(':checked')) {
+    $jq('#formfield-form-widgets-actions_to_close_out').hide();
   }
-  $('#form-widgets-is_this_item_closed-0').change(function () {
-    if( $('#form-widgets-is_this_item_closed-0').is(':checked') ) {
-          $('#formfield-form-widgets-actions_to_close_out').show();
-      }
-      else {
-          $('#formfield-form-widgets-actions_to_close_out').hide();
-      }
+  $jq('#form-widgets-is_this_item_closed-0').change(function () {
+    if ($jq('#form-widgets-is_this_item_closed-0').is(':checked')) {
+      $jq('#formfield-form-widgets-actions_to_close_out').show();
+    }
+    else {
+      $jq('#formfield-form-widgets-actions_to_close_out').hide();
+    }
   });
 
-  $('#form-widgets-related_sow_section').on( "change", function() {
-    if ($( this ).val() != '--NOVALUE--' )     {
-      $searchval = $('body').attr("data-portal-url") + '/@search?fullobjects=1&UID='+ $( this ).val() ;
+  $jq('#form-widgets-related_sow_section').on("change", function () {
+    if ($jq(this).val() != '--NOVALUE--') {
+      $jqsearchval = $jq('body').attr("data-portal-url") + '/@search?fullobjects=1&UID=' + $jq(this).val();
 
-      $.ajax({
-        url: $searchval,
+      $jq.ajax({
+        url: $jqsearchval,
         contentType: "application/json",
         dataType: 'json',
-        headers: {'Accept': 'application/json'},
-        success: function(result){
+        headers: { 'Accept': 'application/json' },
+        success: function (result) {
           //console.log(result.items[0]);
-          $('#sow_text_add').remove();
-          $( "#formfield-form-widgets-related_sow_section").append('<div id="sow_text_add"><hr/>' + result.items[0].bodytext.data + '</div>');
+          $jq('#sow_text_add').remove();
+          $jq("#formfield-form-widgets-related_sow_section").append('<div id="sow_text_add"><hr/>' + result.items[0].bodytext.data + '</div>');
         }
       })
-  }
-});
+    }
+  });
 });
