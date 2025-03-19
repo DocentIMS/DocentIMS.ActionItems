d9eb1aed77963233d770cea12d8eb3480bfec18b
Espen Moe-Nilssen <espen@medialog.no>
2025-03-19 21:06:29 +0100

    adding toolbar 'stuff'

--- src/DocentIMS/ActionItems/viewlets/tool_bar_viewlet.py
+++ src/DocentIMS/ActionItems/viewlets/tool_bar_viewlet.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from plone.app.layout.viewlets import ViewletBase
+from plone import api
 
 
 class ToolBarViewlet(ViewletBase):
@@ -10,6 +11,53 @@ class ToolBarViewlet(ViewletBase):
 
     def get_message(self):
         return u'My message'
+    
+    def tasks_red(self):
+        user_ids = self.current_user_id()
+        items = self.context.portal_catalog.unrestrictedSearchResults(portal_type=['action_items'], urgency="Red", assigned_to = user_ids)
+        ## TO DO: limit to 9
+        return len(items)
+    
+    def tasks_green(self):
+        user_ids = self.current_user_id()
+        items = self.context.portal_catalog.unrestrictedSearchResults(portal_type=['action_items'], urgency="Green", assigned_to = user_ids)
+        ## TO DO: limit to 9
+        return len(items)
+    
+    def tasks_yellow(self):
+        user_ids = self.current_user_id()
+        items = self.context.portal_catalog.unrestrictedSearchResults(portal_type=['action_items'], urgency="Yellow", assigned_to = user_ids)
+        ## TO DO: limit to 9
+        return len(items)
 
+    def notifications_red(self):
+        return 0
+    
+    def notifications_green(self):
+        return 4
+    
+    def notifications_yellow(self):
+        return 3
+    
+    def color(self):
+        return api.portal.get_registry_record('DocentIMS.ActionItems.interfaces.IDocentimsSettings.color1')
+ 
+    def project_name(self):
+        return api.portal.get_registry_record('DocentIMS.ActionItems.interfaces.IDocentimsSettings.project_short_name')     
+    
+    def portal_url(self):
+        return  api.portal.get().absolute_url()   
+    
+    def current_user_id(self):
+        current_user =  api.user.get_current()
+        return current_user.getId()
+    
+    # def red_count(self): 
+    #     user_ids = self.current_user.getId()
+    #     return self.context.portal_catalog.unrestrictedSearchResults(portal_type=['action_items'], urgency="Red", assigned_to = user_ids)
+                     
+    
     def index(self):
         return super(ToolBarViewlet, self).render()
+    
+
