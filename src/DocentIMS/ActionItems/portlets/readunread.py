82e12082e37851923ed449560c7187d398921d4d
Espen Moe-Nilssen <espen@medialog.no>
2025-05-08 08:49:48 +0200

    add show portlet

--- src/DocentIMS/ActionItems/portlets/readunread.py
+++ src/DocentIMS/ActionItems/portlets/readunread.py
@@ -19,10 +19,9 @@ import six.moves.urllib.request
 
 class IReadunreadPortlet(IPortletDataProvider):
     place_str = schema.TextLine(
-        title=_(u'Name of your place with country code'),
-        description=_(u'City name along with country code i.e Delhi,IN'),  # NOQA: E501
+        title=_(u'Name of'),
         required=True,
-        default=u'delhi,in'
+        default=u'Show'
     )
 
 
@@ -30,31 +29,29 @@ class IReadunreadPortlet(IPortletDataProvider):
 class Assignment(base.Assignment):
     schema = IReadunreadPortlet
 
-    def __init__(self, place_str='delhi,in'):
+    def __init__(self, place_str='Show'):
         self.place_str = place_str.lower()
 
     @property
     def title(self):
-        return _(u'Weather of the place')
+        return _(u'Show')
 
 
 class AddForm(base.AddForm):
     schema = IReadunreadPortlet
     form_fields = field.Fields(IReadunreadPortlet)
-    label = _(u'Add Place weather')
-    description = _(u'This portlet displays weather of the place.')
+    label = _(u'Add Portlet') 
 
     def create(self, data):
         return Assignment(
-            place_str=data.get('place_str', 'delhi,in'),
+            place_str=data.get('place_str', 'Show'),
         )
 
 
 class EditForm(base.EditForm):
     schema = IReadunreadPortlet
     form_fields = field.Fields(IReadunreadPortlet)
-    label = _(u'Edit Place weather')
-    description = _(u'This portlet displays weather of the place.')
+    label = _(u'Edit Show') 
 
 
 class Renderer(base.Renderer):
@@ -77,31 +74,6 @@ class Renderer(base.Renderer):
     def available(self):
         """Show the portlet only if there are one or more elements and
         not an anonymous user."""
-        return not self.anonymous and self._data()
+        return not self.anonymous
 
-    def weather_report(self):
-        self.result = self._data()
-        return self.result['description']
-
-    def get_humidity(self):
-        return self.result['humidity']
-
-    def get_pressure(self):
-        return self.result['pressure']
-
-    @memoize
-    def _data(self):
-        baseurl = 'https://query.yahooapis.com/v1/public/yql?'
-        yql_query = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="{0}")'.format(  # NOQA: E501
-            self.data.place_str,
-        )
-        yql_url = baseurl + six.moves.urllib.parse.urlencode(
-            {'q': yql_query},
-        ) + '&format=json'
-        result = six.moves.urllib.request.urlopen(yql_url).read()
-        data = json.loads(result)
-        result = {}
-        result['description'] = data['query']['results']['channel']['description']  # NOQA: E501
-        result['pressure'] = data['query']['results']['channel']['atmosphere']['pressure']  # NOQA: E501
-        result['humidity'] = data['query']['results']['channel']['atmosphere']['humidity']  # NOQA: E501
-        return result
+   
