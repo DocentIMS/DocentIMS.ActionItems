bbcec1366698cabf9e5d4c471a5e4b5fc7394568
Espen Moe-Nilssen <espen@medialog.no>
2025-07-09 19:46:36 +0200

    add mail to roundcube

--- src/DocentIMS/ActionItems/views/member_view.py
+++ src/DocentIMS/ActionItems/views/member_view.py
@@ -6,6 +6,7 @@ from zope.interface import Interface
 from plone.dexterity.utils import iterSchemata
 from zope.schema import getFields
 from plone import api
+from urllib.parse import urlparse
 
 
 # ## -*- coding: utf-8 -*-
@@ -41,6 +42,21 @@ class MemberView(BrowserView):
 
     # def all_users(self):
     #     return api.user.get_users()
+    
+    
+    def get_webmail_url(self):
+        portal_url =  api.portal.get().absolute_url()   
+        parsed_url = urlparse(portal_url)
+        hostname = parsed_url.hostname  # "test.myurl.com"
+
+        # Get the domain part (strip subdomains)
+        domain_parts = hostname.split('.')
+        if len(domain_parts) >= 2:
+            domain = '.'.join(domain_parts[-2:])  # e.g., "myurl.com"
+        else:
+            domain = hostname  # fallback
+
+        return domain
 
     @property
     def group_users(self):
