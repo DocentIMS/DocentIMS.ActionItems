56febd4c48439e3dfc1b0e7f41af2d2092957deb
Espen Moe-Nilssen <espen@medialog.no>
2025-08-11 11:59:22 +0200

    Better Audit view

--- src/DocentIMS/ActionItems/views/permissions_overview.py
+++ src/DocentIMS/ActionItems/views/permissions_overview.py
@@ -10,13 +10,9 @@ from Products.CMFCore.utils import getToolByName
 from plone import api
 from DocentIMS.ActionItems.interfaces import IDocentimsSettings
 from plone.api.exc import InvalidParameterError
-
- 
 from AccessControl.SecurityManagement import getSecurityManager, setSecurityManager
 
- 
-
-# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+#from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class IPermissionsOverview(Interface):
@@ -24,12 +20,19 @@ class IPermissionsOverview(Interface):
 
  
 class PermissionsOverview(BrowserView):
+
+    def __call__(self):
+        self.ikons = self.get_typeicons()
+        self.portal = self.get_portal()
+        self.portal_url = self.get_portal_url()
+        return self.index()
+        
     
-    def portal(self):
+    def get_portal(self):
         return api.portal.get()
     
-    def portal_url(self):
-        return api.portal.get().absolute_url()
+    def get_portal_url(self):
+        return self.portal.absolute_url()
 
     def get_user_permissions(self, obj, user_id):
         user = api.user.get(username=user_id)
@@ -63,7 +66,7 @@ class PermissionsOverview(BrowserView):
     
     def get_available_workflow_actions(self, obj, user=None):
         """Returns workflow actions available to a user on an object"""
-        portal = self.portal()
+        portal = self.portal
         wf_tool = getToolByName(portal, 'portal_workflow')
         
         # Switch to the context of the user, if a specific user is provided
@@ -85,34 +88,54 @@ class PermissionsOverview(BrowserView):
             raw = ''
         return [u.strip() for u in raw.split(',') if u.strip()]
 
-    def get_icon_for_portal_type(self, portal_type):  
+    def get_typeicons(self):  
         # TO Do: Probably better to look up all contenttypes and their icons instead of doing this again and again.
-        content_name = "plone.icon.contenttype/" + portal_type.lower()
-        try:      
-            return api.portal.get_registry_record(content_name)
-        except (KeyError, InvalidParameterError):
-            return '++plone++bootstrap-icons/file-earmark-text.svg'
-            
+        icons = dict()
+        catalog = api.portal.get_tool("portal_catalog")
+        # Query only the portal_type field
+        types_in_catalog = catalog.uniqueValuesFor('portal_type')
+        for the_type in types_in_catalog:
+            type_lower =  the_type.lower().replace(" ", "_")   
+            # Icons are defined in xml in registry / content type and does not contain spaces or Capital letters
+            icon = "plone.icon.contenttype/" + type_lower            
+            try:      
+                icons[the_type] = api.portal.get_registry_record(icon)
+            except (KeyError, InvalidParameterError):
+                # Portal type does not have an icon
+                icons[the_type] =  '++plone++bootstrap-icons/file-earmark-text.svg'
+        return icons
+                
     def get_results(self):        
         # Get the plone_layout view
         request = self.request
         uid = request.get('folder', None)
         if not uid:
-            brains = api.content.find(context=self.context)       
+            folder = self.context
+            brains = api.content.find(folder)       
         else:
             folder = api.content.get(UID=uid)    
             if not folder:
                 return None
             brains = api.content.find(context=folder, sort_on='getObjPositionInParent', sort_order='descending')
+        
+        
+        folder_path_lenght = len(folder.getPhysicalPath())
             
         user_ids = self.get_users()
         results = []
         
         for brain in brains:
             obj = brain.getObject()
-            ikon = self.get_icon_for_portal_type(brain.portal_type)
             path =  obj.getPhysicalPath()
-            row = {'title': obj.Title(), 'ikon': ikon, 'portal_type': obj.portal_type, 'path':  path, 'indent': len(path), 'url': obj.absolute_url(), 'users': {}}            
+            the_type = obj.Type()
+            portal_type = obj.portal_type
+            row = {'title': obj.Title(), 
+                   'ikon': self.ikons[portal_type], 
+                   'portal_type': the_type, 
+                   'path':  path, 
+                   'indent': len(path) - folder_path_lenght, 
+                   'url': obj.absolute_url(), 
+                   'users': {}}            
             for user_id in user_ids:
                 user_id = user_id
                 if user_id:
@@ -127,9 +150,7 @@ class PermissionsOverview(BrowserView):
     
     def get_folders(self):
         catalog = api.portal.get_tool('portal_catalog')
-
         results = catalog(sort_on='sortable_title', portal_type=['Folder'])
-
         items = []
         for brain in results[:100]:  # limit for performance
             items.append({
