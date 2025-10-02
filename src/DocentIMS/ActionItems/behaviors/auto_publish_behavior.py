2152936245800b696eadba8812b49e8817cfd0bf
Espen Moe-Nilssen <espen@medialog.no>
2025-10-02 13:46:57 +0200

    Fix for auto publish behavior

--- src/DocentIMS/ActionItems/behaviors/auto_publish_behavior.py
+++ src/DocentIMS/ActionItems/behaviors/auto_publish_behavior.py
@@ -5,35 +5,67 @@ from plone.autoform.interfaces import IFormFieldProvider
 from plone.supermodel import model
 from plone.autoform import directives as form
 from z3c.form.interfaces import IAddForm
-# from Products.CMFCore.utils import getToolByName
 from zope.component import adapter
-from zope.interface import Interface
-# Register the vocabulary function
-# from zope.schema.vocabulary import VocabularyRegistry
+from zope.interface import Interface 
+from zope.interface import provider
+from Products.CMFCore.utils import getToolByName
+from AccessControl import getSecurityManager
+from zope.schema.interfaces import IVocabularyFactory
+from zope.interface import provider
 
-# from zope.schema.interfaces import IVocabularyFactory
-from zope.interface import implementer
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+
+from Products.CMFCore.utils import getToolByName
+from zope.interface import provider
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary
+
+@provider(IContextSourceBinder)
+def transitionStates(context):
+    wf_tool = getToolByName(context, 'portal_workflow')
+    request = getattr(context, 'REQUEST', None)
+
+    # Default: no portal_type
+    portal_type = None
+
+    if request is not None:
+        # Look at the URL to infer type being added
+        path_info = request.get('PATH_INFO', '')
+        if '++add++' in path_info:
+            portal_type = path_info.split('++add++')[-1]
+    
+    terms = []
+    terms.append(SimpleTerm(value=None, title='Choose'))
+    if portal_type:
+        chains = wf_tool.getChainForPortalType(portal_type)
+        if chains:
+            wf = wf_tool[chains[0]]
+            for t in wf.transitions.values():
+                terms.append(SimpleTerm(value=t.id, title=t.actbox_name or t.id))
+
+    return SimpleVocabulary(terms)
 
 
 # Marker interface
-class IAutoPublishBehaviorMarker(Interface):
+class IAutoPublishBehaviorMarker(Interface):    
     pass
 
 @provider(IFormFieldProvider)
 class IAutoPublishBehavior(model.Schema):
-    """Behavior to auto publish content"""
-
+    """Behavior to auto publish content"""              
     form.mode(transition_state='hidden')
     form.mode(IAddForm, transition_state='input')
     transition_state = schema.Choice(
-        title=u"State",
-        required=True,
-        vocabulary="DocentIMS.ActionItems.TransitionVocabulary"
+        title= u"State",
+        required= True,
+        source=transitionStates,
     )
 
 @implementer(IAutoPublishBehavior)
 @adapter(IAutoPublishBehaviorMarker)
 class AutoPublishBehavior:
     def __init__(self, context):
-        self.context = context
-
+        self.context = context 
+  
+            
