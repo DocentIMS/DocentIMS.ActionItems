99ca8691bef29477df320d60603b78c9a9e428df
Espen Moe-Nilssen <espen@medialog.no>
2025-10-02 11:33:08 +0200

    Add tabbed docmanager view

--- src/DocentIMS/ActionItems/views/tabbed_docmanager_view.py
+++ src/DocentIMS/ActionItems/views/tabbed_docmanager_view.py
@@ -4,6 +4,7 @@
 from Products.Five.browser import BrowserView
 from zope.interface import Interface
 from collections import defaultdict
+from Missing import Value as MissingValue
 
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -21,9 +22,10 @@ class TabbedDocmanagerView(BrowserView):
         # Implement your own actions:
         return self.index()
     
-    def grouped_items_by_document_type(context):
+    def grouped_items_by_document_type(self):
         """Return items in current folder grouped by document_type index."""
-        catalog = context.portal_catalog
+        context = self.context
+        catalog =  context.portal_catalog
         
         # Get brains for items in *this folder only*
         brains = catalog(
@@ -34,13 +36,16 @@ class TabbedDocmanagerView(BrowserView):
         
         for brain in brains:
             doc_type = getattr(brain, 'document_type', None)  # index value
-            if doc_type is None:
+            if doc_type is None or doc_type is MissingValue:
                 doc_type = "Unknown"
             obj = brain.getObject()
             groups[doc_type].append(obj)
         
         # Convert to list of dicts (to preserve your requested format)
+        # And sort them
         return [
-            {"document_type": doc_type, "items": items}
-            for doc_type, items in groups.items()
-        ]
+                {"document_type": doc_type, "items": items}
+                for doc_type, items in sorted(groups.items(), key=lambda x: (x[0] or "").lower())
+            ]
+        
+        # return returnlist
