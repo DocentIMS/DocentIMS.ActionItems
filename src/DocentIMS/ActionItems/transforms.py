0aa91d6a485aadb2ee05e42ed20b1e6906d6a742
Espen Moe-Nilssen <espen@medialog.no>
2025-10-16 16:26:36 +0200

    hide transform

--- src/DocentIMS/ActionItems/transforms.py
+++ src/DocentIMS/ActionItems/transforms.py
@@ -3,23 +3,23 @@ from zope.interface import implementer
 import markdown
 
 
-@implementer(ITransform)
-class QuarkToHTML:
-    __name__ = "quark_to_html"
-    inputs = ("text/x-web-markdown",)
-    output = "text/html"
+# @implementer(ITransform)
+# class QuarkToHTML:
+#     __name__ = "quark_to_html"
+#     inputs = ("text/x-web-markdown",)
+#     output = "text/html"
     
-    import pdb; pdb.set_trace()
+#     import pdb; pdb.set_trace()
 
-    def name(self):
-        return self.__name__
+#     def name(self):
+#         return self.__name__
     
 
-    def convert(self, orig, data, **kwargs):
-        return 'data'
+#     def convert(self, orig, data, **kwargs):
+#         return 'data'
 
 
-def register():
-    return QuarkToHTML()
+# def register():
+#     return QuarkToHTML()
 
   
