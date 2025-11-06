01148ae21ed09e27db0b0cd9e0219ccb17dad2d3
Espen Moe-Nilssen <espen@medialog.no>
2025-11-06 09:22:15 +0100

    Add word properties subscriber

--- src/DocentIMS/ActionItems/subscribers/set_word_properties.py
+++ src/DocentIMS/ActionItems/subscribers/set_word_properties.py
@@ -5,24 +5,23 @@ from docx import Document
 def handler(obj, event):
     """ Event handler
     """
-    # print(u"{0} on object {1}".format(event.__class__, obj.absolute_url()))
-    # 1 check if file is word
-    # 2 if word, change properties
-    # save 
-    import pdb; pdb.set_trace()
+    ## updates word properties (where the file came from)
     file_field = getattr(obj, "file", None)
     # check if file is word
-    if file_field and not getattr(file_field, "data", None):
-        file_data = file_field.data
-        filename = getattr(file_field, "filename", obj.getId()) or "attachment"
-        if filename.lower().endswith(".docx"):
+    
+    if file_field and getattr(file_field, "contentType", None) == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' :
+            # if file_field.contentType == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+            file_data = file_field.data
+            filename = getattr(file_field, "filename", obj.getId()) or "attachment"
             try:
                 doc = Document(BytesIO(file_data))
                 props = doc.core_properties
                 props.subject = obj.absolute_url()
                 buffer = BytesIO()
                 doc.save(buffer)
-                file_data = buffer.getvalue()
-            except Exception as e:
-                pass
+                obj.file.data = buffer.getvalue()
                 
+            
+            except Exception as e:
+                    pass
+                    
